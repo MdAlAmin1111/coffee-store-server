557de2d3afe0d329ca1a64233618235acdb71be2
Md Al-Amin <mdalamin.iubian@gmail.com>
2025-08-17 11:32:13 +0600

    added some functionality of add coffee

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -27,12 +27,39 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        const dataBase = client.db('COFFEE_DB');
+        const coffeeCollection = dataBase.collection('coffees');
+
+        app.get('/coffees', async (req, res) => {
+            // const cursor = coffeeCollection.find();
+            // const result = await cursor.toArray();
+            const result = await coffeeCollection.find().toArray();
+
+            res.send(result);
+        })
+
+        app.post('/coffees', async (req, res) => {
+            const newCoffee = req.body;
+
+            // Insert the defined document into the "movies" collection
+            const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result);
+        })
+
+        app.delete('/coffees/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await coffeeCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        // // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        // // Ensures that the client will close when you finish/error
+        // await client.close();
     }
 }
 run().catch(console.dir);
